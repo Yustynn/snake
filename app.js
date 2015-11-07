c06e81c7457ad44c99ff3e82c540535dc4a54167
Yustynn <yustynn@gmail.com>
2015-11-07 16:03:45 -0500

    Plan app in comments, set up initail HTML index file

---
+++ app.js
@@ -0,0 +1,23 @@
+//  20 x 20 canvas
+// on food eat
+  // snake increases in length (snake.length += 1)
+  // OPTIONAL: game speed increases
+  // new food appears at random location
+    // ensure new location is not inside snake
+
+// on keypress (wsad)
+  // change snake.direction to new direction
+
+// on snake hit wall / self
+  // die.
+
+// keep track of score
+
+// update snake movement very slowly
+
+// black canvas
+// white snake
+// white food
+
+// snake init to 4 x 1
+//
